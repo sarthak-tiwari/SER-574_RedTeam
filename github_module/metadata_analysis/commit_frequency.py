ddfced017189f1bd952bbf6e51fb6716d5c7666b
Ruben Acuna <racuna1@asu.edu>
2019-02-13 23:42:26 -0700

    S#6-TASK#16: Improved documented assumptions.

--- github_module/metadata_analysis/commit_frequency.py
+++ github_module/metadata_analysis/commit_frequency.py
@@ -10,11 +10,14 @@ __author__    = "Ruben Acuna"
 __copyright__ = "Copyright 2019, SER574 Red Team"
 
 
-def count_in_internal(github_repo, username, interval_start, interval_end):
+def count_in_internal(git_repo, username, interval_start, interval_end):
     """
-    Returns the number of commits made by a user in a git repository during a time internal. The interval is inclusive.
+    Returns the number of commits made by a user in a git repository during a
+    time internal. The interval is inclusive.
 
-    :param github_repo: name of a git repository (string).
+    Assumes valid git repo and username.
+
+    :param git_repo: name of a git repository (string).
     :param username: username (string)
     :param interval_start: day for start of internal (date object).
     :param interval_end: day for end of internal (date object).
@@ -23,10 +26,14 @@ def count_in_internal(github_repo, username, interval_start, interval_end):
     raise NotImplementedError
 
 
-def count_on_day(github_repo, username, date):
+def count_on_day(git_repo, username, date):
     """
-    Returns the number of commits made by a usre in a git repository on a specific day.
-    :param github_repo: name of a git repository (string).
+    Returns the number of commits made by a usre in a git repository on a
+    specific day.
+
+    Assumes valid git repo and username.
+
+    :param git_repo: name of a git repository (string).
     :param username: username (string)
     :param date: day (date object).
     :return: number of commits (integer).
@@ -34,10 +41,14 @@ def count_on_day(github_repo, username, date):
     raise NotImplementedError
 
 
-def count_list_internal(github_repo, username, interval_start, interval_end):
+def count_list_internal(git_repo, username, interval_start, interval_end):
     """
-    Returns a list of commits made each day by a user in a git repository during a time internal.The interval is inclusive.
-    :param github_repo: name of a git repository (string).
+    Returns a list of commits made each day by a user in a git repository
+    during a time internal.The interval is inclusive.
+
+    Assumes valid git repo and username.
+
+    :param git_repo: name of a git repository (string).
     :param username: username (string)
     :param interval_start: day for start of internal (date object).
     :param interval_end: day for end of internal (date object).
