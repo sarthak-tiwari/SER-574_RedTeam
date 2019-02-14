c1029b6e520026d5d3a3f48d52c3cfb0b3703c6d
Ruben Acuna <racuna1@asu.edu>
2019-02-13 23:14:22 -0700

    US#6-TASK#16: Designed backend API for frequency analysis.

---
+++ github_module/metadata_analysis/commit_frequency.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+Implementation of an internal API for analyzing frequency of commits for a user
+in a git project.
+"""
+
+
+__author__    = "Ruben Acuna"
+__copyright__ = "Copyright 2019, SER574 Red Team"
+
+
+def count_in_internal(github_repo, username, interval_start, interval_end):
+    """
+    Returns the number of commits made by a user in a git repository during a time internal. The interval is inclusive.
+
+    :param github_repo: name of a git repository (string).
+    :param username: username (string)
+    :param interval_start: day for start of internal (date object).
+    :param interval_end: day for end of internal (date object).
+    :return: number of commits (integer).
+    """
+    raise NotImplementedError
+
+
+def count_on_day(github_repo, username, date):
+    """
+    Returns the number of commits made by a usre in a git repository on a specific day.
+    :param github_repo: name of a git repository (string).
+    :param username: username (string)
+    :param date: day (date object).
+    :return: number of commits (integer).
+    """
+    raise NotImplementedError
+
+
+def count_list_internal(github_repo, username, interval_start, interval_end):
+    """
+    Returns a list of commits made each day by a user in a git repository during a time internal.The interval is inclusive.
+    :param github_repo: name of a git repository (string).
+    :param username: username (string)
+    :param interval_start: day for start of internal (date object).
+    :param interval_end: day for end of internal (date object).
+    :return: a list of commits per day (list of integers).
+    """
+    raise NotImplementedError
