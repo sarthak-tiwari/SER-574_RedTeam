d6019a6cac80599ed2758eb0f0bbbf08b7c78a3e
Ruben Acuna <racuna1@asu.edu>
2019-02-14 23:59:05 -0700

    US#99-TASK#25: First outline for backend API for checking if a pull request has been handled correctly.

---
+++ github_module/metadata_analysis/pull_request.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""
+01234567890123456789012345678901234567890123456789012345678901234567890123456789
+Implementation of an internal API for analyzing pull requests to determine if
+they have been handled correctly.
+
+
+"""
+
+
+__author__    = "Ruben Acuna"
+__copyright__ = "Copyright 2019, SER574 Red Team"
+
+def get_merged_status(github_repo):
+    """
+    Returns a merged pull requestion dictionary which contains
+    :param github_repo:
+    :return: status for each merged pull request (dictionary)
+    """
+    raise NotImplementedError
+
+def get_pull_requests(github_repo):
+    """
+    Returns a list of all pull requests that have been made in a project.
+
+    :param github_repo: name of a github repository (string).
+    :return: list of pull requests (list of integers).
+    """
+    raise NotImplementedError
+
+def get_merged_pull_requests(github_repo):
+    """
+    Returns a list of all pull requests which have been merged.
+
+    Assumes valid github repo and pull request number.
+
+    :param github_repo: name of a github repository (string).
+    :return: list of pull requests (list of integers)
+    """
+    raise NotImplementedError
+
+def handled_correctly(github_repo, pr_num):
+    """
+    Returns a boolean to indicate if a particular pull request was handled correctly.
+    
+    Assumes valid github repo and pull request number. 
+    :param github_repo: name of a github repository (string).
+    :param pr_num: pull request number. (integer)
+    :return: if a pull request was handled correctly (boolean)
+    """
+    raise NotImplementedError
+
+
+# The following are internal functions.
+
+def __get_pr_rules(github_repo, branch):
+    """
+    Finds and returns the current rules for handling pull requests in a specific branch.
+
+    Returns a dictionary which contains the following keys:
+      reviewers_required
+
+    :param github_repo: name of a github repository (string).
+    :param branch: name of a branch in git.
+    :return: rules for handling pull requests (dictionary)
+    """
+    raise NotImplementedError
