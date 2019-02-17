6511fb75bac6f16d8bf681f2329ecebb5dc5d074
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2019-02-17 14:29:00 -0700

    US#32-TASK38: Updated the CheckStyleManager to include function to filter out .java files and return the metrices.

--- github_module/static_code_analysis/CheckStyleManager.py
+++ github_module/static_code_analysis/CheckStyleManager.py
@@ -9,24 +9,42 @@ import subprocess
 class CheckStyleManager:
 
     @staticmethod
-    def getStaticComplexityMetrices():
+    def getStaticComplexityMetrices(fileName):
 
         command = ['java', '-jar', 'checkstyle-8.17-all.jar', '-c',
-                   './custom_checks.xml', './DummyTestFiles/Frame_81.java']
-        output = subprocess.run(command, capture_output=True, universal_newlines=True).stdout
+                   './custom_checks.xml', './DummyTestFiles/' + str(fileName)]
+        output = subprocess.run(command, capture_output=True,
+                                universal_newlines=True).stdout
 
-        metrics = {'BooleanExpressionComplexity':0, 'ClassFanOutComplexity':0, 'CyclomaticComplexity':0,
-                   'JavaNCSS':0, 'NPathComplexity':0, 'ClassDataAbstractionCoupling':0}
+        metrics = {'BooleanExpressionComplexity':0, 'ClassFanOutComplexity':0,
+                   'CyclomaticComplexity':0, 'JavaNCSS':0, 'NPathComplexity':0,
+                   'ClassDataAbstractionCoupling':0}
 
-        for line in output.split('\n'):
+        for line in output.splitlines():
 
             if(line[1:6] == "ERROR"):
                 complexityParameter = line[line.rfind('[')+1:line.rfind(']')]
-                complexityMeasure = line[line.find(' is ')+4:line.find(' (max allowed is')]
+                complexityMeasure = line[line.find(' is ')+4:
+                                         line.find(' (max allowed is')]
         
-                metrics[complexityParameter] = max(metrics[complexityParameter], int(complexityMeasure))
+                metrics[complexityParameter] = max(metrics[complexityParameter]
+                                                   , int(complexityMeasure))
 
         return metrics
 
-metrics = CheckStyleManager.getStaticComplexityMetrices()
-print(metrics)
+
+
+    @staticmethod
+    def getComplexities(filenames):
+
+        result = []
+
+        for filename in filenames:
+            if(filename[filename.rfind('.')+1:] == 'java'):
+                metrics = CheckStyleManager.getStaticComplexityMetrices(filename)
+                result.append((filename, metrics))
+
+        return result
+
+metrices = CheckStyleManager.getComplexities(['Frame_81.java', 'Panel_12.class', 'Panel_59.java'])
+print(metrices)
