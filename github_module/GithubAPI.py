613bb506c39cd50180568fa1d7857f79c23fd7e4
Josh <jkdrumm1@gmail.com>
2019-02-17 22:10:30 -0700

    US: 1, Task: 46 Added functionality to get all commits from a repo, and to get a commit given the repository ID and the commit's SHA

--- github_module/GithubAPI.py
+++ github_module/GithubAPI.py
@@ -35,4 +35,19 @@ def get_repo(repo_id):
     raw_content = json.loads(raw_content.content)
     return raw_content
 
-print(get_repo("168214867"))
+def get_all_commits(repo_id):
+    endpoint = 'https://api.github.com/repositories/' + repo_id + '/commits'
+    raw_content = requests.get(endpoint)
+    raw_content = json.loads(raw_content.content)
+    return raw_content
+
+def get_commit(repo_id, commit_sha):
+    endpoint = 'https://api.github.com/repositories/' + repo_id + '/commits/' + commit_sha
+    return process_get_request(endpoint)
+
+def process_get_request(endpoint):
+    return json.loads(requests.get(endpoint).content)
+
+#print(get_repo("168214867"))
+#print(get_all_commits("168214867"))
+#print(get_commit("168214867", "70f13b111e1147611b70f9c9f1f76ddb00fcbe27"))
