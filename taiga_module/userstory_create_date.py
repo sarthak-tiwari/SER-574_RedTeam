af79a74820ec8c188fb0d0d57e1c95c70cba2a67
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2019-02-18 16:05:39 -0700

    User Story #7 Task #21 (returning output as json)

--- taiga_module/userstory_create_date.py
+++ taiga_module/userstory_create_date.py
@@ -2,9 +2,30 @@ import os
 import requests
 import json
 import getpass
+
 from datetime import datetime as dt
 
+
 http = 'https://api.taiga.io/api/v1/'
+
+def get_userstory_subject(headers,data,userStories):
+	
+	dic = {}
+	lst = []
+	
+	for user_story in userStories:
+		dic["subject"] = user_story["subject"]
+		created = dt.strptime(user_story["created_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
+		created = dt.strftime(created, "%b %d %Y")
+		dic["created on"] = created
+		lst.append(dic)
+		dic = {}
+
+	dic = {}
+	for i in range(len(lst)):
+		dic["User Story " + str(i+1)] = lst[i]
+
+	return json.dumps(dic)
 
 header = {'Content-Type': 'application/json'}
 
@@ -25,78 +46,49 @@ while True:
     except:
         print("Invalid username or password\n")
 		
-while True:
-    try:
-        slug = input("Enter slug: \n")
+projectinfo = "https://api.taiga.io/api/v1/projects/by_slug?slug="
+slug = input("Enter the project slug : ")
+res = requests.get(http +"projects/by_slug",headers = header, params={"slug": slug})
+test = res.json()
 
-        res = requests.get(http +"projects/by_slug",headers = header, params={"slug": slug})
-        test = res.json()
-
-        if test["name"]:
-            name = test["name"]
-            id = test["id"]
-        break
-
-    except:
-        with open("project_dump.json", "w") as outfile:
-            json.dump(test, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
-        print("Invalid slug\n")
+if test["name"]:
+	name = test["name"]
+	test_id = test["id"]
 		
 print("Project Name: {}\n".format(name))
-
-try:
-    res = requests.get(http + "milestones", headers = header, params = {"project": id})
-    sprint = res.json()
-
-    sprintVal = []
-
-    for sprint_name in sprint:
-        sprintVal.append(
-            {
-                "Name": sprint_name["name"]
-            }
-        )
-
-except:
-    with open("sprints_dump.json", "w") as outfile:
-        json.dump(sprint, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
+	
+res = requests.get(http + "milestones", headers = header, params = {"project": test_id})
+sprint = res.json()
+	
+sprintVal = []
+	
+for sprint_name in sprint:
+	sprintVal.append(
+		{
+			"Name": sprint_name["name"]
+		}
+	)
 		
 sprintVal = list(reversed(sprintVal))
 
 print("Sprints are:\n")
-
+	
 for sprints in sprintVal:
-    print("Title: {}".format(sprints["Name"]))
-   
-while True:
-    try:
-        sprintNo = int(input("Enter sprint number: "))
-        if sprintNo >= 1 and sprintNo <= len(sprintVal):
-            break
-        else:
-            print("Sprint does not exsist\n")
-    except:
-        print("Sprint does not exsist\n")
+	print("Title: {}".format(sprints["Name"]))
 		
-
+sprintNo = int(input("Enter sprint number: "))
+if sprintNo <= 1 and sprintNo >= len(sprintVal):
+	print("Sprint does not exsist\n")
+		
+		
 sprintNo = len(sprintVal) - sprintNo
 
 sprintId = sprint[sprintNo]
-id = sprintId["id"]
-
-try:
-    res = requests.get(http + "userstories", headers = header, params = {"milestone": id})
-    userStories = res.json()
-
-    print("\nUser Stories are:\n")
-
-    for user_story in userStories:
-        print("Subject: {}".format(user_story["subject"]))
-        created = dt.strptime(user_story["created_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
-        created = dt.strftime(created, "%b %d %Y")
-        print("Created on: {}".format(created))
-       
-
-except:
-    with open("us_dump.json", "w") as outfile:
-        json.dump(userStories, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
+sprint_id = sprintId["id"]
+		
+res = requests.get(http + "userstories", headers = header, params = {"milestone": sprint_id})
+userStories = res.json()
+		
+subject = get_userstory_subject(header,data,userStories)
+print (subject)
+		
