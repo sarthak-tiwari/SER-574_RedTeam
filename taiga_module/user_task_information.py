ec09cf156e91031bd318ba15d2fbc77d56959165
Srajangpt1 <sgpt18@gmail.com>
2019-02-20 19:48:43 -0700

    Sprint #1 US#2,#16 Task#27,#28 Returning values in Json format for the info collected

--- taiga_module/user_task_information.py
+++ taiga_module/user_task_information.py
@@ -41,21 +41,22 @@ data = {"password": password1,
 
 response = requests.post('https://api.taiga.io/api/v1/auth', headers=headers, data=json.dumps(data))
 data=json.loads(response.content)
-#print(data)
 print("Auth Token : " + data['auth_token'])
 """key = "this is the secret key"
 
 sym_key = SYMKey(key=key, alg="A128KW")
 token=JWE().decrypt(data['auth_token'], keys=[sym_key])
 print(token)"""
+#############################
+projectInfo_bySlug,project_id =  project_info_byslug(headers,data)
+print projectInfo_bySlug
 
-memberinfo,project_id = project_info_byslug(headers,data)
-print memberinfo
+#print rdic
 
 """projectinfo = "https://api.taiga.io/api/v1/projects/by_slug?slug="
 
 project_slug = raw_input("Enter the project slug : ")
-
+memberno = 1
 response_project_data = requests.get(projectinfo+project_slug, headers=headers, data=json.dumps(data))
 project_data = json.loads(response_project_data.content)
 
@@ -68,10 +69,12 @@ for i in project_data['members']:
 	print "  Role          " + i['role_name']"""
 
 
-projec_milestone = "https://api.taiga.io/api/v1/milestones?project=" + str(project_id)
+def user_story_info(project_id,headers,data):
+
+	projec_milestone = "https://api.taiga.io/api/v1/milestones?project=" + str(project_id)
 #projec_milestone = "https://api.taiga.io/api/v1/projects/by_slug?slug="
-response_milestone = requests.get(projec_milestone, headers=headers, data=json.dumps(data))
-milestone_data = json.loads(response_milestone.content)
+	response_milestone = requests.get(projec_milestone, headers=headers, data=json.dumps(data))
+	milestone_data = json.loads(response_milestone.content)
 #id = milestone_data['id']
 #print milestone_data
 #projec_milestone = "https://api.taiga.io/api/v1/milestones"
@@ -80,68 +83,94 @@ milestone_data = json.loads(response_milestone.content)
 #milestone_data = json.loads(response_milestone.content)
 #mdata = response_milestone.json()
 #print mdata
+	dic = {}
+	lst = []
 
-sprintno = 0
-for i in milestone_data:
-	print("Sprint info -")
-	print(str(sprintno) + " : " + i['name'])
-	print("    Created:    " + i['created_date'])
-	print("    Start Date:     " + i['estimated_start'])
-	print("    End Date:      " + i['estimated_finish'])
-	print("    Sprint Points    " + str(i['total_points']))
-	print("    Closed Points    " + str(i['closed_points']))
-	sprintno += 1
+	sprintno = 0
+	for i in milestone_data:
+		print("Sprint info -")
+		print(str(sprintno) + " : " + i['name'])
+		print("    Created:    " + i['created_date'])
+		print("    Start Date:     " + i['estimated_start'])
+		print("    End Date:      " + i['estimated_finish'])
+		print("    Sprint Points    " + str(i['total_points']))
+		print("    Closed Points    " + str(i['closed_points']))
+		sprintno += 1
+	sprintdata = raw_input("Choose a sprint, type in the number the number mentioned for the sprint: ")
 
-sprintdata = raw_input("Choose a sprint, type in the number the number mentioned for the sprint: ")
 
+	sprint_data = milestone_data[int(sprintdata)]
+	#print(sprint_data['name'])
+	#print("User story info for " + sprint_data['name'])
 
-sprint_data = milestone_data[int(sprintdata)]
-print(sprint_data['name'])
-print("User story info for " + sprint_data['name'])
-
-us = []
-
-for i in sprint_data['user_stories']:
+	us = []
+	for i in sprint_data['user_stories']:
 	#print i
-	print "US name:      " + i['subject']
-	print "US Created:     " + str(i['created_date'])
-	print "US Finished:    " + str(i['is_closed'])
-	us.append(i['id'])
-	response_usid = requests.get("https://api.taiga.io/api/v1/history/userstory/" + str(i['id']), headers=headers, data = json.dumps(data))
-	us_data = json.loads(response_usid.content)
-	for j in us_data:
-		diff = j['diff']
-		ms = 'milestone'
-		if ms in diff:
-			diff2 = diff['milestone']
-			if diff2[1] == i['milestone']:
-				print("         Added in sprint on : " + str(j['created_at']))
+		#print "US name:      " + i['subject']
+		#print "US Created:     " + str(i['created_date'])
+		#print "US Finished:    " + str(i['is_closed'])
+		#print "US F date:      " + str(i['finish_date'])
+		#print "---------"
+		us.append(i['id'])
+		dic['US_name'] = i['subject']
+		dic['US_created_date'] = i['created_date']
+		dic['US_finish_date'] = i['finish_date']
+		response_usid = requests.get("https://api.taiga.io/api/v1/history/userstory/" + str(i['id']), headers=headers, data = json.dumps(data))
+		us_data = json.loads(response_usid.content)
+		for j in us_data:
+			diff = j['diff']
+			ms = 'milestone'
+			if ms in diff:
+				diff2 = diff['milestone']
+				if diff2[1] == i['milestone']:
+					#print("         Added in sprint on : " + str(j['created_at']))
+					dic['US_movedToSprintDate'] = j['created_at']
+		lst.append(dic)
+		dic = {}
 
-response_sprintTask = requests.get("http://api.taiga.io/api/v1/tasks?project=" + str(project_id) , headers=headers, data=json.dumps(data))
-sprintTask_data = json.loads(response_sprintTask.content)
+	dic = {}
+	for i in range(len(lst)):
+		dic['US ' + str(i+1)] = lst[i]
 
-task_per_user = []
-for i in sprintTask_data:
-	if i['user_story'] in us:
-		extra = i['assigned_to_extra_info']
-		print(i['subject'] + " is assigned to: " + extra['full_name_display'])
-		print("User Task Created:" + i['created_date'] )
-		task_per_user.append(extra['full_name_display'])
+	return json.dumps(dic),us
 
-user_unique = []
-
-for i in task_per_user:
-	if i not in user_unique:
-		user_unique.append(i)
-
-
-for i in user_unique:
-	print "User " + i + " has " + str(task_per_user.count(i)) + " tasks."
-
-rget = requests.get("http://api.taiga.io/api/v1/userstories/project=" + str(project_id), headers=headers, data = json.dumps(data))
-rdata = json.loads(rget.content)
-
-print rdata
+us_info,us = user_story_info(project_id,headers,data)
+print us_info
+print "----------"
 
 
+def user_task_info(headers,data,us):
+	response_sprintTask = requests.get("http://api.taiga.io/api/v1/tasks?project=" + str(project_id) , headers=headers, data=json.dumps(data))
+	sprintTask_data = json.loads(response_sprintTask.content)
+	dic = {}
+	lst = []
+	task_per_user = []
+	for i in sprintTask_data:
+		if i['user_story'] in us:
+			extra = i['assigned_to_extra_info']
+			#print(i['subject'] + " is assigned to: " + extra['full_name_display'])
+			#print("User Task Created:" + i['created_date'] )
+			task_per_user.append(extra['full_name_display'])
+			dic['user_task'] = i['subject']
+			dic['user_task_created_at'] = i['created_date']
+			lst.append(dic)
+			dic = {}
+
+	dic = {}
+	for i in range(len(lst)):
+		dic['User_task ' + str(i+1)] = lst[i]
+
+	return json.dumps(dic)
+
+	"""user_unique = []
+	for i in task_per_user:
+		if i not in user_unique:
+			user_unique.append(i)
+
+
+	for i in user_unique:
+		print "User " + i + " has " + str(task_per_user.count(i)) + " tasks."""
+
+userTask_info = user_task_info(headers,data,us)
+print userTask_info
 
