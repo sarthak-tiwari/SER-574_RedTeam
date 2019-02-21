d71e727ec9e1d2a779b9d2bb62f0834209a17826
rishabmantri <rishab.mantri@gmail.com>
2019-02-21 16:40:23 -0700

    Modified code to return json data.

--- taiga_module/teammemberinfo.py
+++ taiga_module/teammemberinfo.py
@@ -1,66 +1,59 @@
 import requests
 import json
 
-header = {
+headers = {
     'Content-Type': 'application/json',
 }
+slug1 = "sarthak-tiwari-ser-574_redteam_team-taiga"
 
-user_name = raw_input("username : ")
-password = raw_input("password : ")
-
-
-data = {"password": password, 
-		"type": "normal",        
-		"username": user_name }
-
-API= 'https://api.taiga.io/api/v1/'
-response = requests.post(API+'auth', headers=header, data=json.dumps(data))
-data=json.loads(response.content)
-
-print("Token - " + data['auth_token'])
-
-project_name = raw_input("Enter project name : ")
-
-response = requests.get(API+'projects/by_slug?slug=' +project_name, headers=header, data=json.dumps(data))
-data = json.loads(response.content)
-
-for pdata in data['members']:
-	 
-	print ("Member's Name - " + pdata['full_name_display'])
-	print ("email - " + pdata['email'])
-	print ("Pid - " + pdata['id'])
-	print ("Role - " + pdata['role_name'])
-	print ("Username - " + pdata['username'])
-	print ("__________________________________________________")
+def members_info(slug1):
+	projectinfo = "https://api.taiga.io/api/v1/projects/by_slug?slug="
+	response_project_data = requests.get(projectinfo+slug1, headers=headers)
+	project_data = json.loads(response_project_data.content)
 	
-projec_milestone = "https://api.taiga.io/api/v1/milestones?project=" + str(project_id)
-response_milestone = requests.get(projec_milestone, headers=headers, data=json.dumps(data))
-milestone_data = json.loads(response_milestone.content)
+	dic = {}
+	lst = []
+	for pdata in project_data['members']:
+		dic['full_name_display'] = pdata['full_name_display']
+	 	dic['id'] = pdata['id']
+		dic['role_name'] = pdata['role_name']
+		dic['username'] = pdata['username']
+		lst.append(dic)
+		dic = {}
 
-sprintdata = raw_input("Choose a sprint, type in the number the number mentioned for the sprint: ")
-
-
-sprint_data = milestone_data[int(sprintdata)]
-print(sprint_data['name'])
-print("User story info for " + sprint_data['name'])
-
-us = []
-
-for i in sprint_data['user_stories']:
-	#print i
-	print "US name:      " + i['subject']
-	print "US storyPoints:      " + i['total_points']
-	us.append(i['id'])
-	response_usid = requests.get("https://api.taiga.io/api/v1/history/userstory/" + str(i['id']), headers=headers, data = json.dumps(data))
-	us_data = json.loads(response_usid.content)
+	dic = {}
+	for i in range(len(lst)):
+		dic['User_details ' + str(i+1)] = lst[i]
 	
-response_sprintTask = requests.get("http://api.taiga.io/api/v1/tasks?project=" + str(project_id) , headers=headers, data=json.dumps(data))
-sprintTask_data = json.loads(response_sprintTask.content)
+	return dic
 
-task_per_user = []
-for i in sprintTask_data:
-	if i['user_story'] in us:
-		extra = i['assigned_to_extra_info']
-		print(i['subject'] + " is assigned to: " + extra['full_name_display'])
-		task_per_user.append(extra['full_name_display'])
+def user_story_points_info(slug1,sprint_no):
+	projectinfo = "https://api.taiga.io/api/v1/projects/by_slug?slug="
+	response_project_data = requests.get(projectinfo+slug1, headers=headers)
+	project_data = json.loads(response_project_data.content)
+	project_id = project_data['epic_statuses'][0]['project_id']
+	projec_milestone = "https://api.taiga.io/api/v1/milestones?project=" + str(project_id)
+	response_milestone = requests.get(projec_milestone, headers=headers)
+	milestone_data = json.loads(response_milestone.content)
+	dic = {}
+	lst = []
+	sprint_data = milestone_data[sprint_no]
 
+	us = []
+	for i in sprint_data['user_stories']:
+		us.append(i['id'])
+		dic['US_name'] = i['subject']
+		dic['US_points'] = i['total_points']
+		# dic['US_created_date'] = i['created_date']
+
+		response_usid = requests.get("https://api.taiga.io/api/v1/history/userstory/" + str(i['id']), headers=headers)
+		us_data = json.loads(response_usid.content)
+
+		lst.append(dic)
+		dic = {}
+
+	dic = {}
+	for i in range(len(lst)):
+		dic['US ' + str(i+1)] = lst[i]
+
+	return dic
