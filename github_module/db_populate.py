3984fd16d0c2343b7f2cc0a3b91ba19c5c2e43f7
Josh <jkdrumm1@gmail.com>
2019-03-21 18:46:12 -0700

    US: 66, Task: 73 Allowed the target branch name to be entered into the database

--- github_module/db_populate.py
+++ github_module/db_populate.py
@@ -85,12 +85,12 @@ def store_pull_data(repo_id, pull_no):
     author = data["user"]["login"]                                # TEXT
     request_title = data["body"]
     no_of_comments = data["review_comments"]
-    target_branch = "some branch"
 
     # Might need to change DB to have both base and head branch names
     # Not sure which one target_branch should be for the time being
     base_branch = data["base"] # Usually master
-    head_branch = data["haed"] # Merges into the base
+    head_branch = data["head"] # Merges into the base
+    target_branch = head_branch["label"]
     no_of_reviews = 4
     # query = "INSERT INTO pull_data(requestID, requestTitle, author, noOfComments, " \
     #                                "targetBranch, noOfReviews )" \
