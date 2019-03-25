22fae09d473dbb0f9e61e08b771b27df2a79b298
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2019-03-24 18:13:23 -0700

    US#33-TASK#41: Added the function in CheckStyleManager to count java standard code violations, added sun standard checks too

--- API_Launcher.py
+++ API_Launcher.py
@@ -1,13 +1,12 @@
-# Class to launch github api service on flask
+# Class to launch api service on flask
 #
-# Author: Sarthak Tiwari, Ruben Acuna
-# E-Mail: sarthak.tiwari@asu.edu, racuna1@asu.edu
+# Author: Sarthak Tiwari
+# E-Mail: sarthak.tiwari@asu.edu
 
-from flask import Flask, request
-import datetime
-import json
+from flask import Flask
 
 from github_module.GitHubAPI_Launcher import github_api
 
 app = Flask(__name__)
+
 app.register_blueprint(github_api, url_prefix='/github')
--- github_module/static_code_analysis/CheckStyleManager.py
+++ github_module/static_code_analysis/CheckStyleManager.py
@@ -6,6 +6,7 @@
 
 import subprocess
 
+
 class CheckStyleManager:
 
     @staticmethod
@@ -13,7 +14,6 @@ class CheckStyleManager:
 
         newFile = open('./TestFile/' + fileName, 'w')
         newFile.write(fileContent)
-
 
     @staticmethod
     def getStaticComplexityMetrices(fileName):
@@ -23,9 +23,9 @@ class CheckStyleManager:
         output = subprocess.run(command, capture_output=True,
                                 universal_newlines=True).stdout
 
-        metrics = {'BooleanExpressionComplexity':0, 'ClassFanOutComplexity':0,
-                   'CyclomaticComplexity':0, 'JavaNCSS':0, 'NPathComplexity':0,
-                   'ClassDataAbstractionCoupling':0}
+        metrics = {'BooleanExpressionComplexity': 0, 'ClassFanOutComplexity': 0,
+                   'CyclomaticComplexity': 0, 'JavaNCSS': 0, 'NPathComplexity': 0,
+                   'ClassDataAbstractionCoupling': 0}
 
         for line in output.splitlines():
 
@@ -33,13 +33,24 @@ class CheckStyleManager:
                 complexityParameter = line[line.rfind('[')+1:line.rfind(']')]
                 complexityMeasure = line[line.find(' is ')+4:
                                          line.find(' (max allowed is')]
-        
-                metrics[complexityParameter] = max(metrics[complexityParameter]
-                                                   , int(complexityMeasure))
+
+                metrics[complexityParameter] = max(
+                    metrics[complexityParameter], int(complexityMeasure))
 
         return metrics
 
+    @staticmethod
+    def getJavaWarningCount(fileName):
 
+        command = ['java', '-jar', 'checkstyle-8.17-all.jar', '-c',
+                   './sun_checks.xml', './DummyTestFiles/' + str(fileName)]
+        output = subprocess.run(command, capture_output=True,
+                                universal_newlines=True).stdout
+
+        countLine = output.splitlines()[len(output.splitlines())-1]
+        count = countLine[countLine.find('with ')+5:countLine.find(' error')]
+
+        return count
 
     @staticmethod
     def getComplexities(repoName, filenames):
@@ -48,19 +59,26 @@ class CheckStyleManager:
 
         for filename in filenames:
             if(filename[filename.rfind('.')+1:] == 'java'):
-                metrics = CheckStyleManager.getStaticComplexityMetrices(filename)
+                metrics = CheckStyleManager.getStaticComplexityMetrices(
+                    filename)
+                metrics['javaWarnings'] = CheckStyleManager.getJavaWarningCount(
+                    filename)
                 result.append((filename, metrics))
 
         return result
 
     @staticmethod
-    def getDummyComplexities(filename):
+    def getBaselineForComplexities():
 
-        metrics = {'BooleanExpressionComplexity':2, 'ClassFanOutComplexity':10,
-                   'CyclomaticComplexity':3, 'JavaNCSS':50, 'NPathComplexity':3,
-                   'ClassDataAbstractionCoupling':6}
+        #TODO: Return baseline values for different complexities
+
+        metrics = {'BooleanExpressionComplexity': 2, 'ClassFanOutComplexity': 10,
+                   'CyclomaticComplexity': 3, 'JavaNCSS': 50, 'NPathComplexity': 3,
+                   'ClassDataAbstractionCoupling': 6}
 
         return metrics
 
-# metrices = CheckStyleManager.getComplexities('abc', ['Frame_81.java', 'Panel_59.java'])
+
+# metrices = CheckStyleManager.getComplexities(
+#    'abc', ['Frame_81.java', 'Panel_59.java'])
 # print(metrices)
