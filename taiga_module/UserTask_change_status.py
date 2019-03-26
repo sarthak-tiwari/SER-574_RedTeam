fd6c7d0a43a7f64b882d7e4858775ca9b27593dc
rishabmantri <rishab.mantri@gmail.com>
2019-03-26 09:59:07 -0700

    US#12 task#56 User task status change to Test

--- taiga_module/UserTask_change_status.py
+++ taiga_module/UserTask_change_status.py
@@ -22,6 +22,25 @@ def user_task_info(slug1,sprint_no):
 		if (sprint_no == int(i['milestone_slug'][7])):
 			ut.append(i['id'])
 			utChangeDate[i['id']] = i['created_date']
-  return utChangeDate
+	print "User task status info"
+	for i in range(len(ut)):
+		status_change_dates = {}
+		tempList = []
+		test1 = "https://api.taiga.io/api/v1/history/task/" + str(ut[i])
+		testUserTask1 = requests.get(test1 , headers=headers)
+		testData1 = json.loads(testUserTask1.content)
+		for testEntry in testData1:
+			for key in testEntry['values_diff']:
+				if key == 'status':
+					tempList.append(testEntry['values_diff'][key])
+					tempList.append(testEntry['created_at'])
+		dic['status_change'] = tempList
+		tl = []
+		tl.append(dict([("created_date", utChangeDate[ut[i]])]))
+		tl.append(dict([('status_change',tempList)]))
+		utStatus[ut[i]] = tl
+		tempList = []
+		tl = []
+	return utStatus
 
 print (user_task_info("sarthak-tiwari-ser-574_redteam_team-taiga",2))
