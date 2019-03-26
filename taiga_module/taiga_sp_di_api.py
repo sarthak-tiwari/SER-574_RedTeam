df6a661df0cdf9e56e4cf39b39ece4e7777d2a3d
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2019-03-26 16:00:15 -0700

    US #13 Task #72 and #74 -returning data about modification in task assigned to

--- taiga_module/taiga_sp_di_api.py
+++ taiga_module/taiga_sp_di_api.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, request
 import wikiTextParser
 import listWikiContent
 
-import user_story, user_task_information, userstory_create_date, taskassignedto, task_of_userstory, US_Group1, list_sprints, list_userstories, task_finishdate
+import user_story, user_task_information, userstory_create_date, taskassignedto, task_of_userstory, US_Group1, list_sprints, list_userstories, task_finishdate, UT_History_Info, taskAssignedTo_modified
 
 
 app = Flask(__name__)
@@ -100,6 +100,18 @@ def taskFinishdate():
     projectSlug = request.args.get('projectslug')
     userStoryId = request.args.get('userstory_id')
     return jsonify({'TASK': task_finishdate.get_task_finishdate(projectSlug, userStoryId)})
+	
+@app.route('/taiga/taskAssignedTo_modified', methods=['GET'])
+def assignedToModified():
+    projectSlug = request.args.get('projectslug')
+    sprintno = request.args.get('sprint')
+    return jsonify({'assignedToModified': taskAssignedTo_modified.get_modifiedTaskAssignedTo(projectSlug, sprintno)})
+
+@app.route('/taiga/historyOfTasks', methods=['GET'])
+def historyOfTasks():
+    projectSlug = request.args.get('projectslug')
+    sprintno = request.args.get('sprint')
+    return jsonify({'historyOfTasks': UT_History_Info.user_task_info(projectSlug, sprintno)})
 
 
 if __name__ == '__main__':
--- taiga_module/taskAssignedTo_modified.py
+++ taiga_module/taskAssignedTo_modified.py
@@ -8,7 +8,7 @@ headers = {
 }
 #slug1 = "sarthak-tiwari-ser-574_redteam_team-taiga"
 
-def user_task_info(slug1,sprint_no):
+def get_modifiedTaskAssignedTo(slug1,sprint_no):
 	projectinfo = "https://api.taiga.io/api/v1/projects/by_slug?slug="
 	response_project_data = requests.get(projectinfo+slug1, headers=headers)
 	project_data = json.loads(response_project_data.content)
@@ -19,11 +19,11 @@ def user_task_info(slug1,sprint_no):
 	dic = {}
 	ut = []
 	utHistory = {}
-	utCreate_date = {}
+	assigned_to_modified = {}
 	for i in sprintTask_data:
 		if (sprint_no == int(i['milestone_slug'][7])):
 			ut.append(i['id'])
-			utCreate_date[i['id']] = i['created_date']
+			assigned_to_modified[i['id']] = i['assigned_to']
 	for i in range(len(ut)):
 		status_change_dates = {}
 		tmpList = []
@@ -32,19 +32,15 @@ def user_task_info(slug1,sprint_no):
 		testData1 = json.loads(testUserTask1.content)
 		for testEntry in testData1:
 			for key in testEntry['values_diff']:
-				if key == 'status':
+				if key == 'assigned_to':
 					tmpList.append(testEntry['values_diff'][key])
-					tmpList.append(testEntry['created_at'])
-		dic['status_change'] = tmpList
+		dic['assigned_to_modified'] = tmpList
 		tl = []
-		tl.append(dict([("created_date", utCreate_date[ut[i]])]))
-		tl.append(dict([('status_change',tmpList)]))
+		tl.append(dict([('assigned_to_modified',tmpList)]))
 		utHistory[ut[i]] = tl
 		tmpList = []
 		tl = []
-		#print json.dumps(utHistory, indent=4)
-
+	#print (utHistory)
 	return utHistory
 
-#print user_story_info(slug1,2)
-#user_task_info("sarthak-tiwari-ser-574_redteam_team-taiga",2)
+#get_modifiedTaskAssignedTo("sarthak-tiwari-ser-574_redteam_team-taiga",2)
