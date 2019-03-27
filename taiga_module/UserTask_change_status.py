1864b53002c26c43f09f9b200d36d3f34085fbf4
rishabmantri <rishab.mantri@gmail.com>
2019-03-26 23:45:12 -0700

    minor bug fixes

--- taiga_module/UserTask_change_status.py
+++ taiga_module/UserTask_change_status.py
@@ -7,7 +7,8 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-def user_task_info(slug1,sprint_no):
+def UserTaskchangestatus(slug1,sprint_no):
+	sprint_no = int(sprint_no)
 	projectinfo = "https://api.taiga.io/api/v1/projects/by_slug?slug="
 	response_project_data = requests.get(projectinfo+slug1, headers=headers)
 	project_data = json.loads(response_project_data.content)
@@ -71,5 +72,4 @@ def user_task_info(slug1,sprint_no):
 
 	return utStatus
 
-print (user_task_info("sarthak-tiwari-ser-574_redteam_team-taiga",2))
 
