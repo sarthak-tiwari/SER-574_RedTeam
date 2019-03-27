f9d3ef0a37a308360dd74cf210e7b461e02f7117
Chiranjeevi Ramamurthy <cramamu1@asu.edu>
2019-03-27 14:19:03 -0700

    cors issue fixed

--- taiga_module/taiga_sp_di_api.py
+++ taiga_module/taiga_sp_di_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 import US_Group1
 import findSprintGaps
@@ -17,6 +18,7 @@ import wikiTextParser
 import sprintplanningAnalysis
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/taiga/sprint_story_points', methods=['GET'])
