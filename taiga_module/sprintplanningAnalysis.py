81c009d1b03c9bee2c841a035f10ea391015ae4d
Chiranjeevi Ramamurthy <cramamu1@asu.edu>
2019-04-19 21:02:04 -0700

    implemented updated analyzer

--- taiga_module/sprintplanningAnalysis.py
+++ taiga_module/sprintplanningAnalysis.py
@@ -35,6 +35,31 @@ def wiki_analysis(project_slug, wiki_slug):
     for i in range(1, len(wiki_content) + 1):
         print("")
 
+    wiki_content = wikiTextParser.wikiTextParser(project_slug, wiki_slug)
+    print(wiki_content)
+    planning_analysis = []
+    retrospective_analysis = []
+    review_analysis = []
+    analysis = {}
+
+    for i in range(1, len(wiki_content) + 1):
+        meeting_data = wiki_content["MoM-" + str(i)]
+
+        meeting_description = meeting_data["Description"]
+
+        if re.search("Planning", meeting_description[0], re.IGNORECASE):
+            planning_analysis.append(meeting_data["Date"][0])
+        if re.search("Retrospective", meeting_description[0], re.IGNORECASE):
+            retrospective_analysis.append(meeting_data["Date"][0])
+        if re.search("Review", meeting_description[0], re.IGNORECASE):
+            review_analysis.append(meeting_data["Date"][0])
+
+    analysis["sprint_plan_dates"] = planning_analysis
+    analysis["sprint_retrospective_dates"] = retrospective_analysis
+    analysis["sprint_review_dates"] = review_analysis
+    print(analysis)
+    return analysis
+
     return
 
 
